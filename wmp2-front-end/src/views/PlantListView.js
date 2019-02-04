8f3f1d63762f9044ec56c7f6da022993bf84f4d4
croy <roy.christie95@gmail.com>
2019-02-04 12:07:25 -0600

    Added axios get and delete requests to PlantListView

--- wmp2-front-end/src/views/PlantListView.js
+++ wmp2-front-end/src/views/PlantListView.js
@@ -9,10 +9,34 @@ class PlantListView extends React.Component{
         }
     }
 
+    componentDidMount(){
+        axios
+        .get('')
+        .then(res => {
+          this.setState({ plants: res.data })
+        })
+        .catch(err => {
+          console.log('No plants here');
+        })
+      }
+
+    deletePlant = plantId => {
+    axios
+        .delete(`${baseUrl}/plants/${plantId}`)
+        .then(res => {
+        console.log(res);
+        this.setState({ plants: res.data })
+        this.props.history.push('/plants')
+        })
+        .catch(err => {console.log(err) })
+    }
+    
+
     render(){
         return(
             <PlantList 
             plants={this.state.plants}
+            deletePlant={this.deletePlant}
             />
         );
     }
