3451d0010d0a5391c2c08c5baaa1fc0dddcc7abc
croy <roy.christie95@gmail.com>
2019-02-04 11:52:56 -0600

    Added Login to LoginViewjs

--- wmp2-front-end/src/views/LoginView.js
+++ wmp2-front-end/src/views/LoginView.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import Login from '../components/Login';
+
+class LoginView extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <Login 
+            // Needs addUser passed down to it from either store or View state
+            />
+        );
+    }
+}
+
+export default LoginView;
