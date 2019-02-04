862dc49c37a96288f66d8c906cc07325fa36e6b7
croy <roy.christie95@gmail.com>
2019-02-04 11:21:55 -0600

    Created Authenticate component

--- wmp2-front-end/src/components/Authenticate.js
+++ wmp2-front-end/src/components/Authenticate.js
@@ -1,2 +1,28 @@
 import React from 'react';
 
+import PlantListView from '../views/PlantListView';
+
+const authenticate = App => Login => 
+class extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            loggedIn: false
+        }
+    }
+
+    componentDidMount(){
+        if (!localStorage.getItem('user')){
+            this.setState({ loggedIn: false })
+        } else {
+            this.setState({ loggedIn: true })
+        }
+    }
+
+    render(){
+        if (this.state.loggedIn) return <PlantListView />;
+        return <Login />
+    }
+}
+
+export default authenticate;
