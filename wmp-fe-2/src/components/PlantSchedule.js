3142ef5f851e6665adc2abebbe20ca1d0c11b2e9
croy <roy.christie95@gmail.com>
2019-02-07 12:01:19 -0600

    Updated plant schedule button

--- wmp-fe-2/src/components/PlantSchedule.js
+++ wmp-fe-2/src/components/PlantSchedule.js
@@ -10,7 +10,7 @@ function PlantSchedule(props){
                     <h3>I am a {plant.name}</h3>
                     <p>I need to be watered {plant.lastWater}</p>
                     <p>The date I need to be watered next is {plant.nextWater}</p>
-                    {/* <Button onClick={alert(`Water successful! Water this plant next on ${plant.nextWater}`)}>Click to Water!</Button> */}
+                    <Button onClick={() => alert(`Water successful! Water this plant next on ${plant.nextWater}`)}>Click to Water!</Button>
                 </div>
             ))}
         </div>
